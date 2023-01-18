16c16015efd80b27105a04664e6f1dc4c976c5d4
Promit-revar <promit.revar2211@gmail.com>
2023-01-18 12:53:40 +0530

    Created program for Bowling Game and tested it with jest

--- Evaluation1/BallingGame.js
+++ Evaluation1/BallingGame.js
@@ -7,17 +7,19 @@ const CalculateScore=(frames)=>{
         
         let nxtThrow=(!frames[i+1])?0:frames[i+1];
         currFrame=frames[i]+nxtThrow;
-        //console.log(score,frames[i],currFrame);
+        //console.log(score,frames[i],currFrame,i);
         if(frames[i]===10){
         
         let nxtFrame=(!frames[i+1])?0:frames[i+1];
         let nxtToNxtFrame=(!frames[i+2])?0:frames[i+2];
-        console.log(frames[i]+nxtFrame+nxtToNxtFrame,score);
+       // console.log(frames[i]+nxtFrame+nxtToNxtFrame,score);
+
         score+=frames[i]+nxtFrame+nxtToNxtFrame;
         i+=3;
         }
         else if(currFrame===10){
-            let nxtBall=(!frames[i+1])?0:frames[i+1];
+            let nxtBall=(!frames[i+2])?0:frames[i+2];
+            //console.log(10+nxtBall);
             score+=(10+nxtBall);
             i+=2;
         }
@@ -30,5 +32,5 @@ const CalculateScore=(frames)=>{
     }
     return score;
 }
-console.log(CalculateScore([10,5,5,9,0]));
+console.log(CalculateScore([10,0,0,5,5,2,3]));
 module.exports={CalculateScore}
--- Evaluation1/BowlingGameTest.test.js
+++ Evaluation1/BowlingGameTest.test.js
@@ -2,6 +2,10 @@ const {CalculateScore}= require("./BallingGame");
 test("Calculate scores for the frames..", () => {
     expect(CalculateScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
   });
-  test("Calculate scores for the frames..", () => {
+test("Calculate scores for the frames..", () => {
     expect(CalculateScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 10,10,10])).toBe(111);
   });
+  test("Calculate scores for the frames SPARE..", () => {
+    expect(CalculateScore([10,0,0,5,5,2,3])).toBe(27);
+  });
+  
